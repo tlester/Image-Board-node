baeaf1aa3278609eac1b0bf2197dd1c296451ca3
Thomas Lester <tom@tomlester.com>
2016-04-15 12:09:20 -0400

    feat: Build out starting framework
    - generated the proper MVC framework for expres app
    - Built main layout.hbs

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET /test page. */
+router.get('/test', function(req, res, next) {
+  res.render('index', { title: 'Test' });
+});
+
+module.exports = router;
